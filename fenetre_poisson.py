0ddf268a30493c0ca3fe71bb8c29c04443357dab
jeremy lebel <2125813@cegepoutaouais.qc.ca>
2022-05-15 13:41:30 -0400

    supprimer poisson finis

--- fenetre_poisson.py
+++ fenetre_poisson.py
@@ -132,7 +132,6 @@ class Fenetre_poisson(QtWidgets.QDialog, interface_poisson.Ui_Dialog):
     @pyqtSlot()
     # Bouton modifier pour Poisson
     def on_BT_modifier_p_clicked(self):
-        print("a")
         """
         Gestionnaire d'évènement pour le bouton ajouter un poisson
         """
@@ -146,50 +145,48 @@ class Fenetre_poisson(QtWidgets.QDialog, interface_poisson.Ui_Dialog):
         po.Type_eau = self.CB_eau_p.currentText()
         po.Type_alimentation = self.CB_alimentation_p.currentText()
         po.enclos = self.CB_enclos_p.currentText()
-        print("b")
-        # True/False qui nous informe si num d'enclos existe ou pas dans liste des enclos + si type
+        # True/False qui nous informe si num d'enclos existe ou pas dans liste des enclos + si type st == poisson
         verifier_animal = verifier_animal_liste(po.Num_animal)
         verifier_type_animal = verifier_animal_est_poisson(po.Type_animal)
-        # Si num d'enclos valide mais existe déjà dans la liste enclos, on ajoute pas
+        # Si num d'enclos valide mais existe pas dans la liste enclos, message d'erreur
         if verifier_animal is False:
             self.line_num_e.clear()
             self.MS_e_num_existant_p.setVisible(True)
         if verifier_type_animal is False:
             self.MS_e_type_p.setVisible(True)
-        if po.Num_animal == "":  # Si le num d'enclos est invalide, effacer lineEdit et afficher message d'erreur
+        if po.Num_animal == "":  # Si le num est invalide, effacer lineEdit et afficher message d'erreur
             self.line_num_p.clear()
             self.MS_e_num_format_p.setVisible(True)
-        if po.Nom_animal == "":
+        if po.Nom_animal == "": # Si le nom est invalide, effacer lineEdit et afficher message d'erreur
             self.MS_e_nom_format_p.setVisible(True)
             self.MS_e_nom_lenght_p.setVisible(True)
             self.line_nom_p.clear()
-        if po.Longueur_poisson == 0:
+        if po.Longueur_poisson == 0: # Si longueur est invalide, effacer lineEdit et afficher message d'erreur
             self.line_longueur_p.clear()
             self.MS_e_longueur_sup_p.setVisible(True)
             # si num est valide et n'existe pas deja on creer
         if po.Num_animal != "" and po.Nom_animal != "" and po.Longueur_poisson != 0 and verifier_type_animal is True \
-                and verifier_animal is True:
+                and verifier_animal is True: # Si toutes valeurs sont correctes
             for a in lst_animal_globale:
-                if a.Num_animal == po.Num_animal:
+                if a.Num_animal == po.Num_animal: # identifie l'objet poisson correspondant au num entre
                     for enc in lst_enclos:
-                        if enc.Num_enclos == a.enclos.Num_enclos:
-                            enc.lst_animal.remove(a)
+                        if enc.Num_enclos == a.enclos.Num_enclos: # identifie l'objet poisson correspondant a ancien poisson
+                            enc.lst_animal.remove(a) # on supprime l'ancien poisson de son enclos
                     a.Nom_animal = self.line_nom_p.text()
                     a.Longueur_poisson = self.line_longueur_p.text()
-                    a.Type_animal = self.CB_type_animal_p.currentText()
+                    a.Type_animal = self.CB_type_animal_p.currentText() # associe new valeur au bon objet poisson
                     a.Type_alimentation = self.CB_alimentation_p.currentText()
                     a.Type_eau = self.CB_eau_p.currentText()
                     for encl in lst_enclos:
                         if encl.Num_enclos == po.enclos:
-                            a.enclos = encl
+                            a.enclos = encl # modifie l'enclos pour sa nouvelle valeur
             for enclo in lst_enclos:
                 if enclo.Num_enclos == self.CB_enclos_p.currentText():
-                    enclo.lst_animal.append(po)
-            # self.textBrowser_p.clear()  # reinitialisation du text browser
-            self.textBrowser_p.clear()
+                    enclo.lst_animal.append(po) # ajoute poisson maintenant modifie dans la liste d'animaux de l'enclos
+            self.textBrowser_p.clear() # reinitialisation du text browser
             for animaux in lst_animal_globale:
-                if animaux.Type_animal == "Poisson":
-                    self.textBrowser_p.append(animaux.__str__())  # Afffichage de tous les poison dans la liste
+                if animaux.Type_animal == "Poisson": # affiche seulement poisson dans page poisson
+                    self.textBrowser_p.append(animaux.__str__())  # Afffichage de tous les poisons dans la liste
                     self.textBrowser_p.append("")  # Spacer
             self.line_num_p.clear()  # Réinitialiser lineEdits num et combobox de pop up poisson
             self.CB_eau_p.setCurrentText("Salé")
@@ -197,4 +194,45 @@ class Fenetre_poisson(QtWidgets.QDialog, interface_poisson.Ui_Dialog):
             self.line_nom_p.clear()
             self.line_longueur_p.clear()
             self.CB_type_animal_p.setCurrentText("Poisson")
-            print("finis")
+
+    @pyqtSlot()
+    # Bouton modifier pour Poisson
+    def on_BT_supprimer_p_clicked(self):
+        """
+        Gestionnaire d'évènement pour le bouton ajouter un poisson
+        """
+        # Instancier un objet Enclos
+        po = Poisson()
+        # Entrée de donnée pour les attributs de l'objet Poisson
+        po.Num_animal = self.line_num_p.text().capitalize()
+        po.Type_animal = self.CB_type_animal_p.currentText()
+        # True/False qui nous informe si num d'enclos existe ou pas dans liste des enclos + si type st == poisson
+        verifier_animal = verifier_animal_liste(po.Num_animal)
+        verifier_type_animal = verifier_animal_est_poisson(po.Type_animal)
+        # Si num d'enclos valide mais existe pas dans la liste enclos, message d'erreur
+        if verifier_animal is False:
+            self.line_num_e.clear()
+            self.MS_e_num_existant_p.setVisible(True)
+        if verifier_type_animal is False:
+            self.MS_e_type_p.setVisible(True)
+        if po.Num_animal == "":  # Si le num est invalide, effacer lineEdit et afficher message d'erreur
+            self.line_num_p.clear()
+            self.MS_e_num_format_p.setVisible(True)
+        if verifier_type_animal is True and verifier_animal is True:  # Si toutes valeurs sont correctes
+            for a in lst_animal_globale:
+                if a.Num_animal == po.Num_animal:  # identifie l'objet poisson correspondant au num entre
+                    lst_animal_globale.remove(a)
+                    for enc in lst_enclos:
+                        if enc.Num_enclos == a.enclos.Num_enclos: # identifie l'objet poisson correspondant au num entre
+                            enc.lst_animal.remove(a)  # on supprime poisson de son enclos
+            self.textBrowser_p.clear()  # reinitialisation du text browser
+            for animaux in lst_animal_globale:
+                if animaux.Type_animal == "Poisson":  # affiche seulement poisson dans page poisson
+                    self.textBrowser_p.append(animaux.__str__())  # Afffichage de tous les poisons dans la liste
+                    self.textBrowser_p.append("")  # Spacer
+            self.line_num_p.clear()  # Réinitialiser lineEdits num et combobox de pop up poisson
+            self.CB_eau_p.setCurrentText("Salé")
+            self.CB_alimentation_p.setCurrentText("sous-sol")
+            self.line_nom_p.clear()
+            self.line_longueur_p.clear()
+            self.CB_type_animal_p.setCurrentText("Poisson")
